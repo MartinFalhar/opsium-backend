0c701cd4577d0dc34009213643454ca8eccf5e44
Martin Falhar <martin.falhar@gmail.com>
2025-08-03 23:40:23 +0200

    Routes repair repo

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import usersRoutes from "../backend/routes/users.routes.js";
+import usersRoutes from "./routes/users.routes.js";
 import bodyParser from "body-parser";
 import cors from "cors";
 
