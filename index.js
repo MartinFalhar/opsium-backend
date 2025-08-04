cfafd1d602973ef2d242d30cd0e93032bedf9b38
Martin Falhar <martin.falhar@gmail.com>
2025-08-04 20:43:37 +0200

    CORS policy

--- index.js
+++ index.js
@@ -4,7 +4,29 @@ import bodyParser from "body-parser";
 import cors from "cors";
 
 const app = express();
-app.use(cors()); // Enable CORS policyfor all routes
+
+// Seznam domén, kterým chceme povolit přístup
+const allowedOrigins = [
+  "http://localhost:5173", // vývojové prostředí
+  "http://localhost:3000", // pro jistotu
+  "https://opsium-frontend.onrender.com", // produkce
+];
+//ORIGINAL VERSION
+// app.use(cors()); // Enable CORS policy for all routes
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS: " + origin));
+      }
+    },
+    credentials: true,
+  })
+); // Enable CORS policy for all routes
+
 const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
